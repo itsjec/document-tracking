bdc07dd0b0f1cfe11bbd4023d32f47fd64f9c773
Jec <jramos112802@gmail.com>
2023-12-26 15:47:10 +0800

    change base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://docutrack.online/cityhall/';
+    public string $baseURL = 'http://document-tracking.test/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
